fcdcc2fc46e44a14eb5fb8d2797d252e33b1ce1b
JawadKadery <jawadalkadiri@gmail.com>
2026-07-25 18:53:19 +0300

    Add PORT env variable support to server.js

--- server.js
+++ server.js
@@ -43,6 +43,7 @@ app.get('/api/history', (req, res) => {
     res.json(rows);
 })
 
-app.listen(3001, () => {
-    console.log('Server is running on port 3001');
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
